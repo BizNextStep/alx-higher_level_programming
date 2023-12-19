32629f8cc3dc628b315f18ce1d4493611c5d6d24
BizNextStep <charmaine.n.mbatha@icloud.com>
2023-12-19 08:05:16 -0800

    0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a square"""
+
+
+class Square:
+    """This class represents a square"""
+
+    def __init__(self):
+        """This method is the constructor for the Square class"""
+        pass
