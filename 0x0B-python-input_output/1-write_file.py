142ff4dec7bfceaadccbdb467911a55552de2ea6
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-01-09 07:10:18 -0800

    0

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    function that returns the number
+    of lines of a text file:
+"""
+
+
+def number_of_lines(filename=""):
+    """ return the number of lines of a file """
+    i = 0
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            i += 1
+    return i
