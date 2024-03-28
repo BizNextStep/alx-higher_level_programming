71adf9265d1b3a5cade817b1f3df2179a3c0c772
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-03-28 10:57:51 -0700

    updates

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,13 +2,27 @@
 """
 Displays the value of the X-Request-Id variable found in the header of the response.
 """
+
 import urllib.request
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+def get_request_id(url):
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    headers = response.info()
 
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        print(x_request_id)
+    # Check if the 'X-Request-Id' header is present
+    if 'X-Request-Id' in headers:
+        request_id = headers['X-Request-Id']
+        print(f"The value of X-Request-Id is: {request_id}")
+    else:
+        print("X-Request-Id header not found in the response.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    get_request_id(url)
 
